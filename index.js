46039ab235d13700a1013a8e4e880ebdb164bfe0
tommarku <tmarku4@gmail.com>
2023-10-02 12:58:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const companyName = 'Scuber'
+
+let mostProfitableNeighborhood 
+mostProfitableNeighborhood = `Chelsea`
+
+let companyCeo
+companyCeo = `Susan Smith`
